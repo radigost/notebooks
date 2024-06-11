e8f1c61215b42f5e98aa7aa53b4718ccb61f2470
Nikolai Poliakov <nickolay.pol@gmail.com>
2024-06-11 20:24:00 +0300

    removed outdated code

--- speech recognition/speech_recognition.py
+++ speech recognition/speech_recognition.py
@@ -2,22 +2,24 @@ from argparse import ArgumentParser
 from speechkit import model_repository, configure_credentials, creds
 from speechkit.stt import AudioProcessingType
 
-# Аутентификация через API-ключ.
+load_dotenv()
+yandexAPIKey = os.getenv("YANDEX_API")
+
 configure_credentials(
     yandex_credentials=creds.YandexCredentials(
-        api_key='YOUR_API'
+        api_key=yandexAPIKey
     )
 )
 
 def recognize(audio):
     model = model_repository.recognition_model()
 
-    # Задайте настройки распознавания.
+    # settings of speech recognition
     model.model = 'general'
     model.language = 'ru-RU'
     model.audio_processing_type = AudioProcessingType.Full
 
-    # Распознавание речи в указанном аудиофайле и вывод результатов в консоль.
+    # Speech recognition and print it to console
     result = model.transcribe_file(audio)
     for c, res in enumerate(result):
         print('=' * 80)
